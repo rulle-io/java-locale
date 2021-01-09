5c7fc0f0bd4844279aec003257dc08eecd4de29a
Ruslan Altynnikov <>
2021-01-09 16:24:57 +0100

    Add first version of stats.

--- src/main/java/rulle/io/locale/LocaleLabel.java
+++ src/main/java/rulle/io/locale/LocaleLabel.java
@@ -5,4 +5,28 @@ public record LocaleLabel(
   LocaleNumberStrings numbersPart,
   LocaleDateStrings datePart,
   LocaleCurrencyStrings currencyPart
-) {}
+) {
+    @Override
+    public String toString() {
+        StringBuilder buf = new StringBuilder();
+        buf.append(
+                (String.format(
+                        "=== Formatting for Locale: %s [%s]\n",
+                        localePart().localeStr(), localePart().localeDisplayName())));
+
+        buf.append((String.format("Integer: %s\n", numbersPart().integerStr())));
+        buf.append((String.format("Double: %s\n", numbersPart().doubleStr())));
+        buf.append((String.format("Currency: %s [%s]: %s \n",
+                currencyPart().currencyDisplayName(),
+                currencyPart().currencyCode(),
+                currencyPart().currencyStr()
+        )));
+        buf.append((String.format("Date Full: %s\n", datePart().dateFull())));
+        buf.append((String.format("Date Long: %s\n", datePart().dateLong())));
+        buf.append((String.format("Date Medium: %s\n", datePart().dateMedium())));
+        buf.append((String.format("Date Short: %s\n", datePart().dateShort())));
+        buf.append("===");
+        
+        return buf.toString();
+    }
+}
--- src/main/java/rulle/io/locale/LocaleLabelMaker.java
+++ src/main/java/rulle/io/locale/LocaleLabelMaker.java
@@ -1,15 +1,21 @@
 package rulle.io.locale;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.SetMultimap;
 import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.time.temporal.UnsupportedTemporalTypeException;
+import java.util.Arrays;
 import java.util.Currency;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LocaleLabelMaker {
   public static void main(String[] args) {
@@ -25,14 +31,31 @@ public class LocaleLabelMaker {
           Locale.FRANCE,
           Locale.GERMANY,
           Locale.ITALY,
+          Locale.UK,
           LOCALE_SWEDEN,
           LOCALE_RUSSIA,
-          Locale.CHINA
+          Locale.CHINA,
+          Locale.JAPAN,
+          Locale.KOREA
         };
     Locale[] localesAll = Locale.getAvailableLocales();
-    for (Locale locale : locales) {
-      makeLocaleLabel(locale).ifPresent(v -> printLocaleLabel(v));
-    }
+    Stream<Locale> stream = Arrays.stream(localesAll);
+    List<LocaleLabel> labels = stream
+            .map(l -> makeLocaleLabel(l))
+            .flatMap(Optional::stream)
+            .collect(Collectors.toList());
+
+    LocaleStats dateShort = new LocaleStats("dateShort"); 
+    labels.forEach(label -> {
+      printLocaleLabel(label);
+      // TODO: extend to all tracked parameters
+      // status regarding usage of a pattern:
+      // YYYY-MM-DD => [USA, UK, IT]
+      dateShort.put(label.datePart().dateShort(), label.localePart().localeStr());
+    });
+
+    System.out.println("DateLong pattern: " + dateShort.toString());
+    
   }
 
   public static Optional<LocaleLabel> makeLocaleLabel(final Locale currentLocale) {
@@ -41,16 +64,16 @@ public class LocaleLabelMaker {
     if (Objects.isNull(currentLocaleStr) || currentLocaleStr.isEmpty()) {
       return Optional.empty();
     }
-  
+
     // Random input values
     Integer quantity = Integer.valueOf(123456);
     Double amount = Double.valueOf(345987.246);
-    LocalDate localDate = LocalDate.of(2025, Month.JANUARY, 1);
+    LocalDate localDate = LocalDate.of(2025, Month.DECEMBER, 31);
     Double currencyAmount = Double.valueOf(9876543.21);
-    
+
     NumberFormat numberFormatter = NumberFormat.getNumberInstance(currentLocale);
     NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(currentLocale);
-    
+
     String quantityOut = numberFormatter.format(quantity);
     String amountOut = numberFormatter.format(amount);
 
@@ -85,26 +108,47 @@ public class LocaleLabelMaker {
     String localDateStrShort = getFormattedValue(localDate, dateFormatterShort);
 
     LocaleStrings ls = new LocaleStrings(currentLocale.getDisplayName(), currentLocaleStr);
-    LocaleNumberStrings lns = new LocaleNumberStrings(quantityOut, amountOut, currentCurrency.getDisplayName(), currencyOut);
-    LocaleDateStrings lds = new LocaleDateStrings(localDateStrFull, localDateStrLong, localDateStrMedium, localDateStrShort);
-    LocaleCurrencyStrings lcs = new LocaleCurrencyStrings(currentCurrency.getCurrencyCode(), currentCurrency.getDisplayName(),  currencyOut);
-      
+    LocaleNumberStrings lns =
+        new LocaleNumberStrings(
+            quantityOut, amountOut, currentCurrency.getDisplayName(), currencyOut);
+    LocaleDateStrings lds =
+        new LocaleDateStrings(
+            localDateStrFull, localDateStrLong, localDateStrMedium, localDateStrShort);
+    LocaleCurrencyStrings lcs =
+        new LocaleCurrencyStrings(
+            currentCurrency.getCurrencyCode(), currentCurrency.getDisplayName(), currencyOut);
+
     return Optional.of(new LocaleLabel(ls, lns, lds, lcs));
   }
 
-  public static void printLocaleLabel(final LocaleLabel localeLabel) {
-    System.out.println("=== Formatting for Locale: " + localeLabel.localePart().localeStr() + " [" + localeLabel.localePart().localeDisplayName() + "]");
-    System.out.println("Integer: " + localeLabel.numbersPart().integerStr());
-    System.out.println("Double: " + localeLabel.numbersPart().doubleStr());
-    System.out.println("Currency: " + localeLabel.currencyPart().currencyDisplayName() + "-" + localeLabel.currencyPart().currencyCode() + ": " + localeLabel.currencyPart().currencyStr());
-    System.out.println("Date Full: " + localeLabel.datePart().dateFull());
-    System.out.println("Date Long: " + localeLabel.datePart().dateLong());
-    System.out.println("Date Medium: " + localeLabel.datePart().dateMedium());
-    System.out.println("Date Short: " + localeLabel.datePart().dateShort());
+  public static class LocaleStats {
+    private final String name;
+    private final SetMultimap<String, String> stats = HashMultimap.create();
 
-    System.out.println("===");
+    public LocaleStats(final String name) {
+      this.name = name;
+    }
+
+
+    public boolean put(final String metricName, final String metricValue) {
+      return stats.put(metricName, metricValue);
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder buf = new StringBuilder(String.format("LocaleStats [%s]\n", name));
+      stats.asMap().entrySet().forEach(
+              stat ->
+                      buf.append(String.format("[%s] -> [%s]\n", stat.getKey(), stat.getValue().toString())));
+      buf.append(" - === -\n");
+      return buf.toString();
+    }
   }
-  
+
+  public static void printLocaleLabel(final LocaleLabel localeLabel) {
+    System.out.println(localeLabel.toString());
+  }
+
   private static String getFormattedValue(
       LocalDate localDate, DateTimeFormatter dateTimeFormatterFull) {
     try {
@@ -113,4 +157,5 @@ public class LocaleLabelMaker {
       return "N/A";
     }
   }
+  
 }
